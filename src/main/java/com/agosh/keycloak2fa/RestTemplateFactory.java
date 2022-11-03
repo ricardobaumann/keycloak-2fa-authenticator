8e97094fa631afef7464afe43f1e7598522dd95b
ricardo <ricardo.baumann@krieger.de>
2022-11-03 11:17:28 +0100

    integration with notification service

--- src/main/java/com/agosh/keycloak2fa/RestTemplateFactory.java
+++ src/main/java/com/agosh/keycloak2fa/RestTemplateFactory.java
@@ -1,13 +1,16 @@
 package com.agosh.keycloak2fa;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
+@Slf4j
 public class RestTemplateFactory {
-    
+
     public static RestTemplate build(String baseUrl) {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
+        log.info("Building rest template with base URL: {}", baseUrl);
         return restTemplate;
     }
 
